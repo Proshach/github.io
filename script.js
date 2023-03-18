b8afd7f9320032dbcfc62833f569a8f205142f8b
Proshach <93697148+Proshach@users.noreply.github.com>
2023-03-18 21:37:13 +0900

    Update script.js

--- script.js
+++ script.js
@@ -20,3 +20,25 @@ function sayHello() {
 
   fetchImage();
 </script>
+
+ <script>
+    var satellites = document.querySelectorAll(".satellite");
+    var orbit = document.getElementById("orbit");
+    var orbitRadius = orbit.offsetWidth / 2;
+    var angles = [0, 1.5, 3];
+    var speeds = [0.01, 0.03, 0.06];
+
+    function moveSatellite(satellite, angle, speed) {
+      setInterval(function () {
+        angle += speed;
+        var xPosition = orbitRadius * Math.cos(angle);
+        var yPosition = orbitRadius * Math.sin(angle);
+        satellite.style.transform =
+          "translate(" + xPosition + "px, " + yPosition + "px)";
+      }, 80);
+    }
+
+    for (var i = 0; i < satellites.length; i++) {
+      moveSatellite(satellites[i], angles[i], speeds[i]);
+    }
+  </script>
