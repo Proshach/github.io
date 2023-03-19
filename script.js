7ccccfa1464b4cc74df4eb6e32898302ff255b12
Proshach <93697148+Proshach@users.noreply.github.com>
2023-03-20 01:30:28 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,49 +1,61 @@
- /* const satellites = document.querySelectorAll(".satellite");
-const orbit = document.getElementById("orbit");
-const orbitRadius = orbit.offsetWidth / 2;
-
-function moveSatellite(satellite) {
-  const angle = parseFloat(satellite.dataset.angle);
-  const speed = parseFloat(satellite.dataset.speed);
-  const xPosition = orbitRadius * Math.cos(angle);
-  const yPosition = orbitRadius * Math.sin(angle);
-  satellite.style.transform =
-    "translate(" + xPosition + "px, " + yPosition + "px)";
-  satellite.dataset.angle = angle + speed;
-  requestAnimationFrame(() => moveSatellite(satellite));
-}
-
-function setupSatellites() {
-  satellites.forEach(satellite => {
-    moveSatellite(satellite);
-  });
-}
-
-
- */
-setupSatellites();
-
-document.getElementById("nasa-radio").addEventListener("click", function () {
-  window.location.href = "https://apod.nasa.gov/apod/archivepix.html";
-});
-
-function sayHello() {
-  alert("Click on your favorite planet and discover your secret world !");
-}
 
 const apiKey = "hNXu2p56bBRXQnBxzntuVFpCxu07T4gxQNQVS9aQ";
 const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`;
 
-async function fetchImage() {
-  try {
-    const response = await fetch(apiUrl);
-    const data = await response.json();
-    const image = document.createElement("img");
-    image.src = data.hdurl;
-    document.body.appendChild(image);
-  } catch (error) {
-    console.error(error);
-  }
+fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+        const image = document.createElement("img");
+        image.src = data.hdurl;
+        document.body.appendChild(image);
+    })
+    .catch((error) => console.error(error));
+
+
+var satellites = document.querySelectorAll(".satellite");
+var orbit = document.getElementById("orbit");
+var orbitRadius = orbit.offsetWidth / 2;
+var angles = [0, 1.5, 3];
+var speeds = [0.01, 0.03, 0.06];
+
+function moveSatellite(satellite, angle, speed) {
+    setInterval(function () {
+        angle += speed;
+        var xPosition = orbitRadius * Math.cos(angle);
+        var yPosition = orbitRadius * Math.sin(angle);
+        satellite.style.transform =
+            "translate(" + xPosition + "px, " + yPosition + "px)";
+    }, 80);
 }
 
-fetchImage();
+for (var i = 0; i < satellites.length; i++) {
+    moveSatellite(satellites[i], angles[i], speeds[i]);
+}
+// Planets Project
+// Get references to the button and modal
+const explorePlanetsBtn = document.querySelector('#explore-planets-btn');
+const explorePlanetsModal = document.querySelector('#explore-planets-modal');
+const closeModalBtn = document.querySelector('#close-modal-btn');
+
+// Add a click event listener to the button
+explorePlanetsBtn.addEventListener('click', () => {
+    // Show the modal when the button is clicked
+    explorePlanetsModal.classList.add('show');
+});
+
+// Add a click event listener to the close button in the modal
+closeModalBtn.addEventListener('click', () => {
+    // Hide the modal when the close button is clicked
+    explorePlanetsModal.classList.remove('show');
+});
+
+// Add a click event listener to the links in the modal to redirect to the selected planet
+const planetLinks = explorePlanetsModal.querySelectorAll('a');
+planetLinks.forEach(link => {
+    link.addEventListener('click', (event) => {
+        event.preventDefault(); // Prevent the default link behavior
+        const planetName = link.textContent.toLowerCase();
+        window.location.href = `/${planetName}.html`; // Redirect to the selected planet's page
+    });
+});
+
