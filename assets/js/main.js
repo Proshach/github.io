05e7aee2e0d1d33d4357799a9912938d03a5ea6d
Proshark <93697148+Proshach@users.noreply.github.com>
2026-03-11 01:31:12 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,12 @@
 const heroImages = [
-"images/photo1.jpg",
-"images/photo2.jpg",
-"images/photo3.jpg"
+"assets/images/photo1.jpg",
+"assets/images/photo2.jpg",
+"assets/images/photo3.jpg"
 ]
 
 const hero = document.querySelector(".hero")
 
 const randomImage = heroImages[Math.floor(Math.random() * heroImages.length)]
 
-hero.style.backgroundImage = `url(${randomImage})`
+hero.style.backgroundImage =
+`linear-gradient(rgba(0,0,0,0.45), rgba(0,0,0,0.45)), url(${randomImage})`
